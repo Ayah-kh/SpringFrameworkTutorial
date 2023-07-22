84a74aabe83bed676f3f535ceebae72c037990e4
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-07-22 10:20:46 +0300

    First Commit

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class Employee {
+    //Attributes
+    int eId;
+    String eName;
+    String eAddress;
+
+    public Employee() {
+
+    }
+    public Employee(int eId, String eName, String eAddress) {
+        this.eId = eId;
+        this.eName = eName;
+        this.eAddress = eAddress;
+    }
+
+    //setters and getters
+
+    public int geteId() {
+        return eId;
+    }
+
+    public void seteId(int eId) {
+        this.eId = eId;
+    }
+
+    public String geteName() {
+        return eName;
+    }
+
+    public void seteName(String eName) {
+        this.eName = eName;
+    }
+
+    public String geteAddress() {
+        return eAddress;
+    }
+
+    public void seteAddress(String eAddress) {
+        this.eAddress = eAddress;
+    }
+}
