afd6d102df126c38861b887132c513ed92e7ea9b
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-07-22 12:17:30 +0300

    First Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context=new ClassPathXmlApplicationContext("employee.xml");
+        ApplicationContext context=
+                new ClassPathXmlApplicationContext("employee.xml");
 
         Employee e1=(Employee) context.getBean("employee1");
         Employee e2= context.getBean("employee2",Employee.class);
+
+        System.out.println("Employee 1 "+e1);
+        System.out.println("Employee 2 "+e2);
 
     }
 }
