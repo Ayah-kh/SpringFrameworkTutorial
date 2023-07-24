118f21378671c80dd3fe3b2da0113ae55c71dcb8
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-07-24 14:52:53 +0300

    First Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context=
-                new ClassPathXmlApplicationContext("employee.xml");
+                new ClassPathXmlApplicationContext("beans.xml");
 
         Employee e1=(Employee) context.getBean("employee1");
 //        Employee e2= context.getBean("employee2",Employee.class);
