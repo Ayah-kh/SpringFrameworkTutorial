610719a7bba9b5a578c6280c044fc8c32fde0fb0
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-07-28 13:08:30 +0300

    First Commit

--- src/main/java/org/example/Principal.java
+++ src/main/java/org/example/Principal.java
@@ -3,7 +3,6 @@ package org.example;
 public class Principal {
 
     public void principleInfo(){
-        System.out.println("Hi, I am your principle");
-        System.out.println("My name is James Bond");
+        System.out.println("Hi, I am James Bond, your principle");
     }
 }
