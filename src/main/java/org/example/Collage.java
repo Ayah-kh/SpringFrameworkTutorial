feeb741ec3377cc3fb4cd006837c9239d6915e56
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-07-28 10:46:06 +0300

    First Commit

--- src/main/java/org/example/Collage.java
+++ src/main/java/org/example/Collage.java
@@ -2,11 +2,10 @@ package org.example;
 
 import org.springframework.stereotype.Component;
 
-@Component("collegeBean")
+
 public class Collage {
 
     public void test(){
         System.out.println("testing this method");
     }
-
 }
--- src/main/java/org/example/ConfigClass.java
+++ src/main/java/org/example/ConfigClass.java
@@ -1,10 +1,13 @@
 package org.example;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigClass {
 
-
+    @Bean(name = "collBean")
+    public Collage collegeBean(){
+        return new Collage();
+    }
 }
