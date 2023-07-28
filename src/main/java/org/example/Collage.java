61e950061a58a3dd96ce5cd918b570ce6ea8018e
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-07-28 10:57:18 +0300

    First Commit

--- src/main/java/org/example/Collage.java
+++ src/main/java/org/example/Collage.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 
 public class Collage {
 
+    private Principal principal;
+
     public void test(){
         System.out.println("testing this method");
     }
---
+++ src/main/java/org/example/Principal.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Principal {
+
+    public void principleInfo(){
+        System.out.println("Hi, I am your principle");
+        System.out.println("My name is James Bond");
+    }
+}
